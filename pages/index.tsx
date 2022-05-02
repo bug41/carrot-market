ac5c7581101d62ced82cd572acf1eeffb9b67746
bug41 <bugkingus@gmail.com>
2022-05-02 12:16:56 +0900

    dark 모드 설정 방법

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,25 +2,28 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-slate-400 py-20 px-20 flex flex-col space-y-5 min-h-screen">
-      <div className="bg-white p-6 rounded-3xl shadow-xl">
-        <span className="font-semibold text-3xl">Select Item</span>
+    <div className="bg-slate-400 dark xl:place-content-center py-20 px-20 grid gap-10 xl:grid-cols-3 lg:grid-cols-2 min-h-screen">
+      <div className="bg-white dark:bg-black flex flex-col sm:bg-red-400 md:bg-teal-400 lg:bg-indigo-400 xl:bg-yellow-500 2xl:bg-pink-500 p-6 rounded-3xl shadow-xl">
+        <span className="dark:text-white font-semibold text-3xl">Select Item</span>
+        
         <ul>
           {[1,2,3,4,5].map((i) => (
             <div key={i} className="flex justify-between my-2 odd:bg-blue-50 even:bg-yellow-500">
-              <span className="text-gray-500">Grey Chair</span>
-              <span className="font-semibold">$19</span>
+              <span className="text-gray-500 dark:text-white">Grey Chair</span>
+              <span className="font-semibold dark:text-white">$19</span>
             </div>            
           ))}
         </ul>
+        {/*
         <ul>
           {["a","b","c",""].map((c,i) => (
             <li className="bg-red-500 py-2 empty:hidden" key={i}>{c}</li>
           ))}
         </ul>
+        */}
         <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
-          <span>Total</span>
-          <span className="font-semibold">$10</span>
+          <span className="dark:text-white">Total</span>
+          <span className="font-semibold dark:text-white">$10</span>
         </div>
         <button
           className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-3/4 mx-auto
@@ -29,7 +32,7 @@ const Home: NextPage = () => {
       </div>
 
       <div className="bg-white overflow-hidden rounded-3xl shadow-xl group">
-        <div className="bg-blue-600 p-6 pb-14">
+        <div className="landscape:bg-teal-500 portrait:bg-indigo-600 p-6 pb-14 xl:pb-40">
           <span className="text-white text-2xl">Profile</span>
         </div>
         <div className="rounded-3xl p-6 bg-white relative -top-5">
@@ -53,7 +56,7 @@ const Home: NextPage = () => {
         </div>
       </div>
 
-      <div className="bg-white p-6 rounded-3xl shadow-xl">
+      <div className="bg-white p-6 rounded-3xl shadow-xl lg:col-span-2 xl:col-span-1">
         <div className="flex mb-5 justify-between items-center" >
           <span>😊</span>
           <div className="space-x-3">
@@ -83,7 +86,6 @@ const Home: NextPage = () => {
           <button className="bg-blue-500 py-2 px-8 text-center text-xs text-white rounded-lg">Add to cart</button>
         </div>
       </div>
-      <div className="bg-white p-6 rounded-3xl shadow-xl"></div>
     </div>
   )
   
