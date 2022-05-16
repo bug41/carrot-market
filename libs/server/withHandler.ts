ed99439a93a8b07c126751ab806abb06de360579
bug41 <bugkingus@gmail.com>
2022-05-16 21:54:37 +0900

    뭔말이야 이게 스바

--- libs/server/withHandler.ts
+++ libs/server/withHandler.ts
@@ -5,6 +5,15 @@ export default function withHandler(
     fn : (req:NextApiRequest,res:NextApiResponse) => void
 ) {
     return async function(req:NextApiRequest,res:NextApiResponse) {
-        
+        if(req.method !== method){
+            return res.status(405).end();            
+        }
+
+        try{
+            await fn(req,res);
+        }catch(error){
+            console.log(error);
+            return res.status(500).json({error});
+        }
     }
 }
